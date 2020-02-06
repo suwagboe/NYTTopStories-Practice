def96f98fac91d448dd6b9bf2fc6a3de26d320b9
uwagboe <sm.uwagboe@gmail.com>
2020-02-06 11:05:11 -0500

    made the instance of the controller and called them in ViewDidLoad

--- NYTTopStories/controllers/TopStoriesTabController.swift
+++ NYTTopStories/controllers/TopStoriesTabController.swift
@@ -10,13 +10,34 @@ import UIKit
 
 class TopStoriesTabController: UITabBarController {
     
+    private lazy var newsFC: NewsFeedController = {
+        
+        let controller = NewsFeedController()
+        controller.tabBarItem = UITabBarItem(title: "News Feed", image: UIImage(systemName: "eyeglasses"), tag: 0)
+        return controller
+        
+    }()
     
+    private lazy var savedAC: SavedArticleController = {
+         
+         let controller = SavedArticleController()
+         controller.tabBarItem = UITabBarItem(title: "Saved Article", image: UIImage(systemName: "folder"), tag: 1)
+         return controller
+         
+     }()
+    
+    private lazy var settings: SettingsController = {
+         
+         let controller = SettingsController()
+         controller.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 2)
+         return controller
+         
+     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemTeal
         
-        viewControllers = [NewsFeedController(), SavedArticleController(), SettingsController()]
+        viewControllers = [newsFC, savedAC, settings]
     }
     
     
