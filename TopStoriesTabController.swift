9cde876b3fd7ffb96472594df07e861e75588bd5
uwagboe <sm.uwagboe@gmail.com>
2020-02-06 10:48:39 -0500

    Initial Commit

---
+++ TopStoriesTabController.swift
@@ -0,0 +1,20 @@
+//
+//  TopStoriesTabController.swift
+//  NYTTopStories
+//
+//  Created by Pursuit on 2/6/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class TopStoriesTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+    
+    
+    
+}
