21cd02ac8c476c4d64388b1b445e7775f2e45e3b
uwagboe <sm.uwagboe@gmail.com>
2020-02-06 12:52:11 -0500

    added apiClient

--- NYTTopStories/NetworkServices/NYTTopStoriesAPIClient.swift
+++ NYTTopStories/NetworkServices/NYTTopStoriesAPIClient.swift
@@ -7,4 +7,35 @@
 //
 
 import Foundation
+import NetworkHelper
 
+struct NYTTopStoriesAPIClient {
+    static func fetchTopStorties(for section: String, completion: @escaping (Result<[Article], AppError>)-> ()) {
+        let endpointURL = "https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key=..."
+        
+        guard let url = URL(string: endpointURL) else {
+            completion(.failure(.badURL(endpointURL)))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result{
+            case .failure(let appError):
+                // this is where the decoding of the data should happen and return the data
+                completion(.failure(.decodingError(appError)))
+            case .success(let data):
+                do{
+                    // remember to ALWAYS go from the top level and then go deeper inside of the model
+                    let stories = try JSONDecoder().decode(TopStories.self, from: data)
+                    completion(.success(stories.results))
+                }catch{
+                    completion(.failure(.decodingError(error)))
+                }
+                
+            }
+        }
+        
+    }
+}
